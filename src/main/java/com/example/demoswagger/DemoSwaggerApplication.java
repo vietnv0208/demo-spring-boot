0c1a9421de0aec64ac1ae56a3539409f74119c44
VietNV <vietnv@twendeesoft.com>
2023-05-28 23:03:48 +0700

    test build fail

--- src/main/java/com/example/demoswagger/DemoSwaggerApplication.java
+++ src/main/java/com/example/demoswagger/DemoSwaggerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoSwaggerApplication {
 
     public static void main(String[] args) {
+        tét err
         SpringApplication.run(DemoSwaggerApplication.class, args);
     }
 
