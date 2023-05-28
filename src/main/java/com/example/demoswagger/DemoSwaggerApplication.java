17ff32786d8e4eeae055bb0520cf953f3cbf4436
VietNV <vietnv@twendeesoft.com>
2023-05-29 00:09:57 +0700

    test build

--- src/main/java/com/example/demoswagger/DemoSwaggerApplication.java
+++ src/main/java/com/example/demoswagger/DemoSwaggerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoSwaggerApplication {
 
-    public static void main(String[] args) {ff
+    public static void main(String[] args) {
         System.out.println("test");
         SpringApplication.run(DemoSwaggerApplication.class, args);
     }
