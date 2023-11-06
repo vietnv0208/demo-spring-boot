fb2b14517bba8c81742e511948548179c1dad665
Tien Hoang <tien.hoang@commerceship.com>
2023-11-06 17:43:30 +0700

    add test

--- src/test/java/com/example/demoswagger/DemoSwaggerApplicationTests.java
+++ src/test/java/com/example/demoswagger/DemoSwaggerApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.demoswagger;
 
+import java.util.Locale;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.i18n.LocaleContextHolder;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoSwaggerApplicationTests {
 
     @Test
     void contextLoads() {
+        LocaleContextHolder.setLocale(Locale.ENGLISH);
+
+        assertThat("a").isEqualTo("b");
     }
 
 }
