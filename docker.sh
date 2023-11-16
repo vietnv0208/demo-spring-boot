56f37fdd17b99de3b403838194f17b28d87a37eb
Tien Hoang <tien.hoang@commerceship.com>
2023-11-16 17:18:14 +0700

    add debug remote

--- docker.sh
+++ docker.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 docker build -t spring-dev . -f Dockerfile  --target development
 
+#show detail when build
+docker build -t spring-dev . -f Dockerfile  --target development  --progress=plain
+docker build -t spring-dev . -f Dockerfile  --target development  --progress=plain --no-cache
+
 #run
 docker run --publish 8086:8086 spring-dev
 #docker run --publish 8086:8086  --publish 8000:8000  spring-dev #remote debug via 8000
