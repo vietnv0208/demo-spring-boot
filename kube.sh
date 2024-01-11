230a7de07b04fd4fd84ecabfe18a55d444f96300
VietNV <vietnv@twendeesoft.com>
2024-01-11 16:50:25 +0700

    test k8s

---
+++ kube.sh
@@ -0,0 +1,8 @@
+#1
+kubectl apply -f docker-java-kubernetes.yaml
+#2
+kubectl get deployments
+kubectl get services
+#3 check ooutput
+#4 tear down your application
+kubectl delete -f docker-java-kubernetes.yaml
